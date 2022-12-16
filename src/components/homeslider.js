46752de1b1d073f3daf2bbd1a98cd7a4123e6762
thangarajPixel <thangaraj.pixel@gmail.com>
2022-12-16 12:39:16 +0530

    BannerChanges16Dec

--- src/components/homeslider.js
+++ src/components/homeslider.js
@@ -1,26 +1,20 @@
 import React, { Component } from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Carousel from 'react-bootstrap/Carousel';
-import Banner1 from '../images/banners/banner-1.png';
-import Banner2 from '../images/banners/banner-2.png';
-import Banner3 from '../images/banners/banner-3.png';
-import Banner4 from '../images/banners/banner-4.png';
-import Banner1Mobile from '../images/banners/banner-1-mobile.png';
-import Banner2Mobile from '../images/banners/banner-2-mobile.png';
-import Banner3Mobile from '../images/banners/banner-3-mobile.png';
-import Banner4Mobile from '../images/banners/banner-4-mobile.png';
+import Banner1 from '../images/banners/Banner-1.webp';
+import Banner2 from '../images/banners/Banner-2.webp';
+import Banner3 from '../images/banners/Banner-3.webp';
+import Banner4 from '../images/banners/Banner-4.webp';
+import Banner1Mobile from '../images/banners/Banner-Mobile1.webp';
+import Banner2Mobile from '../images/banners/Banner-Mobile2.webp';
+import Banner3Mobile from '../images/banners/Banner-Mobile3.webp';
+import Banner4Mobile from '../images/banners/Banner-Mobile4.webp';
 import { Link } from 'react-router-dom';
 
 
 class Homeslider extends Component {
     render() {
         return (
-            <section className="home-slider-bg">                
-                <Container>
-                    <Row>
-                        <Col>
+            <section className="home-slider-bg">
                             <Carousel fade className="d-none d-lg-block d-md-block">
                                 <Carousel.Item>
                                     <Link to="/packages/">
@@ -65,9 +59,6 @@ class Homeslider extends Component {
                                     </Link>
                                 </Carousel.Item>
                             </Carousel>
-                        </Col>
-                    </Row>
-                </Container>
             </section>
         );
     }
