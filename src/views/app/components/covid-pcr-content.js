758917668e145efb8d50d9c473a22964b71d027f
Fahd <fahd.pixel@gmail.com>
2023-03-22 16:57:16 +0530

    Refactor

--- src/views/app/components/covid-pcr-content.js
+++ src/views/app/components/covid-pcr-content.js
@@ -6,32 +6,31 @@ import Accordion from "react-bootstrap/Accordion";
 import { usePackages } from "hooks/packages";
 
 const CovidPcrContent = (props) => {
-
   const { defaultCity } = props;
-
+  // eslint-disable-next-line
   const [packageData, setPackageData] = useState(null);
+  // eslint-disable-next-line 
   const { mutate: packages, isLoading: loading } = usePackages();
 
   const onFetchPackages = (searchParams) => {
     const nformData = JSON.stringify(searchParams);
     packages(nformData, {
       onSuccess: (data) => {
-        setPackageData(data?.packages)
-      }
+        setPackageData(data?.packages);
+      },
     });
-  }
+  };
 
   useEffect(() => {
     if (defaultCity) {
       const params = {
-        "cityId": defaultCity,
-        "package_name": "Covid"
-      }
+        cityId: defaultCity,
+        package_name: "Covid",
+      };
       onFetchPackages(params);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [defaultCity]);
-
 
   return (
     <>
@@ -46,9 +45,9 @@ const CovidPcrContent = (props) => {
               <p>
                 COVID-19 is viral infection caused by novel corona virus, now
                 called Severe Acute Respiratory. Syndrome Coronavirus-2
-                (SARS-CoV-2). On March 11, 2020, the WHO declared COVID-19 as
-                a global pandemic, which has taken a significant toll on
-                people across the world.
+                (SARS-CoV-2). On March 11, 2020, the WHO declared COVID-19 as a
+                global pandemic, which has taken a significant toll on people
+                across the world.
               </p>
               <h4 className="text-purple pad-top-20 pad-bot-20">
                 To help fight this pandemic, A.G Diagnostics offers:
@@ -74,16 +73,16 @@ const CovidPcrContent = (props) => {
                     </div>
                     <div className="answ">
                       Sample collection is done using a swab to collect
-                      respiratory material from oropharyngeal and
-                      nasopharyngeal area. A swab contains a soft tip on a
-                      long, flexible stick that is inserted into your nose/
-                      throat. After collection, the swab is sealed in a tube
-                      and then sent to a laboratory.
+                      respiratory material from oropharyngeal and nasopharyngeal
+                      area. A swab contains a soft tip on a long, flexible stick
+                      that is inserted into your nose/ throat. After collection,
+                      the swab is sealed in a tube and then sent to a
+                      laboratory.
                       <br />
                       <br />
                       The test can detect very small amounts of virus material
-                      thus, it is the most accurate and reliable test to
-                      detect acute infection in the body.
+                      thus, it is the most accurate and reliable test to detect
+                      acute infection in the body.
                     </div>
                     <div className="ques">What do Test Results signify?</div>
                     <div className="answ border-bottom-0">
@@ -94,8 +93,8 @@ const CovidPcrContent = (props) => {
                       <br />
                       <br />A negative test result means you most probably do
                       not have COVID-19 infection at the time you took your
-                      test. If your test is negative, continue to take steps
-                      to follow COVID safe behaviour like wearing mask, social
+                      test. If your test is negative, continue to take steps to
+                      follow COVID safe behaviour like wearing mask, social
                       distancing, and hand sanitisation.
                     </div>
                   </Accordion.Body>
@@ -106,9 +105,8 @@ const CovidPcrContent = (props) => {
                   </Accordion.Header>
                   <Accordion.Body>
                     <h5 className="text-dark">
-                      To monitor severity of infection while getting treated
-                      for COVID-19 & to identify people in need of care
-                      escalation.
+                      To monitor severity of infection while getting treated for
+                      COVID-19 & to identify people in need of care escalation.
                     </h5>
                     <div className="ques">
                       Why COVID -19 Monitoring tests are important?
@@ -120,9 +118,9 @@ const CovidPcrContent = (props) => {
                       patients may be at high risk for increased D-Dimer
                       associated with formation intravascular blood clots and
                       other associated complications. Thus, while treating
-                      yourself for COVID -19, it is also necessary to monitor
-                      & follow-up comprehensive investigations to understand
-                      the disease severity.
+                      yourself for COVID -19, it is also necessary to monitor &
+                      follow-up comprehensive investigations to understand the
+                      disease severity.
                     </div>
                     <div className="ques">
                       Which tests are crucial for COVID -19 Monitoring?{" "}
@@ -153,8 +151,7 @@ const CovidPcrContent = (props) => {
                     </div>
                     <div className="answ">
                       Antibody test determines if you have had a COVID-19
-                      infection in the past – for Serosurveillance purpose
-                      only.{" "}
+                      infection in the past – for Serosurveillance purpose only.{" "}
                     </div>
                     <div className="ques">
                       Is RT-PCR and Antibody Test different?
@@ -162,15 +159,13 @@ const CovidPcrContent = (props) => {
                     <div className="answ border-bottom-0">
                       Yes. COVID RT-PCR swab test is for specific diagnosis of
                       current active SARS CoV-2 infection whereas, Antibody
-                      tests should not be used to diagnose a current
-                      infection.
+                      tests should not be used to diagnose a current infection.
                       <br />
                       <br />A positive test result for COVID-19 RT PCR test
                       means it is very likely you have COVID-19 infection.{" "}
                       <br />
-                      <br />A positive Antibody test means that you were
-                      likely infected in the past and have developed immune
-                      response.
+                      <br />A positive Antibody test means that you were likely
+                      infected in the past and have developed immune response.
                     </div>
                   </Accordion.Body>
                 </Accordion.Item>
@@ -181,8 +176,8 @@ const CovidPcrContent = (props) => {
                   <Accordion.Body>
                     <h5 className="text-dark">
                       To determine quantitative antibody response post
-                      vaccination (for all types of vaccines). To be done
-                      after 2 weeks after the 2nd dose.
+                      vaccination (for all types of vaccines). To be done after
+                      2 weeks after the 2nd dose.
                     </h5>
                     <div className="ques">
                       Why Anti-SARS-CoV-2 Spike Protein (S1/S2) IgG Test is
@@ -193,13 +188,13 @@ const CovidPcrContent = (props) => {
                       antibodies against S1/S2 spike protein.
                       <br />
                       <br />
-                      Assay provides indication of presence of neutralising
-                      IgG antibodies against SARS-CoV-2, thus of protective
+                      Assay provides indication of presence of neutralising IgG
+                      antibodies against SARS-CoV-2, thus of protective
                       immunity.
                       <br />
                       <br />
-                      Helpful to detect post vaccination immune response to
-                      all types of COVID-19 vaccine.
+                      Helpful to detect post vaccination immune response to all
+                      types of COVID-19 vaccine.
                     </div>
                     <div className="ques">
                       When should you schedule the test?
@@ -207,8 +202,8 @@ const CovidPcrContent = (props) => {
                     <div className="answ border-bottom-0">
                       SARS-CoV-2 IgG Antibodies usually appear after 2-3 weeks
                       of injection or 2 weeks post second dose of vaccination.
-                      Thus, it is suggested to do the test after 2 weeks of
-                      the 2nd dose of COVID Vaccine.
+                      Thus, it is suggested to do the test after 2 weeks of the
+                      2nd dose of COVID Vaccine.
                     </div>
                   </Accordion.Body>
                 </Accordion.Item>
@@ -216,10 +211,9 @@ const CovidPcrContent = (props) => {
             </Col>
           </Row>
         </Container>
-      </section>      
+      </section>
     </>
   );
-}
-
+};
 
 export default CovidPcrContent;
