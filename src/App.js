9ecce16b65f2f8904f1aabdd8003549baf874c41
thangarajPixel <thangaraj.pixel@gmail.com>
2023-04-04 15:41:33 +0530

    CityNameVariablePune

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "../src/assets/css/style.css";
 function App() {
 
   let history = useHistory();
-  localStorage.setItem("city_name", 'Ahmednagar');
+  localStorage.setItem("city_name", 'Pune');
   return (
     <Suspense fallback={""}>
       <Router history={history}>
