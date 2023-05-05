92dc3072a6acf0590892a64f1f53d81667c7e3cc
thangarajPixel <thangaraj.pixel@gmail.com>
2023-05-05 18:45:05 +0530

    url-change-05May23

--- src/services/homeVisitServices/index.js
+++ src/services/homeVisitServices/index.js
@@ -6,7 +6,7 @@ const MILISECONDS = 1000;
 const TIMEOUT = SECONDS * MILISECONDS;
 
 const client = axios.create({
-  baseURL: "https://beta.agdiagnostics.com/admin/api/",
+  baseURL: "https://admin.agdiagnostics.com/api/",
   timeout: TIMEOUT,
   headers: {
     "content-type": "application/json",
