39474203ad582051640eb1bbf820ebd6f398f24c
Fahd <fahd.pixel@gmail.com>
2023-05-16 14:23:47 +0530

    Removed HashRouter

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import AppLayout from "layouts/app-layout";
-import { Switch, HashRouter as Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 export const Views = (props) => {
   const { location } = props;
 
   return (
     <Switch>
-      <Route exact path="">
-        <AppLayout location={location} />
-      </Route>
+      <AppLayout location={location} />
     </Switch>
   );
 };
